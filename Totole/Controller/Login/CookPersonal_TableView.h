10fb636ec60c48eaca8df73cc9f637bd918e81d1
zhuliang <zhuliang424@163.com>
2012-11-26 01:09:01 +0800

    新建cookPersonal类

---
+++ Totole/Controller/Login/CookPersonal_TableView.h
@@ -0,0 +1,18 @@
+//
+//  CookPersonal_TableView.h
+//  Totole
+//
+//  Created by  on 12-11-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface CookPersonal_TableView : UITableView<UITableViewDelegate,UITableViewDataSource>
+{
+    
+}
+
+@end
---
+++ Totole/Controller/Login/CookPersonal_TableView.m
@@ -0,0 +1,24 @@
+//
+//  CookPersonal_TableView.m
+//  Totole
+//
+//  Created by  on 12-11-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CookPersonal_TableView.h"
+
+@implementation CookPersonal_TableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+        
+    }
+    return self;
+}
+
+@end
+
